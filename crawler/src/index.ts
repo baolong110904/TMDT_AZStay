f6f1361703327a72fa142c1ba97eb9fd9cc7bd58
NationalWind <94291098+NationalWind@users.noreply.github.com>
2025-07-16 20:22:39 +0700

    fix index

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from './routes/users.routes';
 import listingsRoutes from './routes/listings.routes';
-import bidsRoutes from './routes/bids.routes';
 import seedRoutes from './routes/seed.routes';
 
 dotenv.config();
@@ -12,9 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRoutes);
 app.use('/listings', listingsRoutes);
-app.use('/bids', bidsRoutes);
 app.use('/api', seedRoutes);
 
 const PORT = process.env.PORT || 4000;
